f05f59b62de977d5f1cf98d0ea376d5fab69845b
Lucille James <lucille.james00@gmail.com>
2020-09-21 11:00:51 +0200

    added Books routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,26 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @bok.destroy
+    # redirect_to root_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :rating, :author_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :author
-  resources :book
+  resources :authors
+  resources :books
 end
