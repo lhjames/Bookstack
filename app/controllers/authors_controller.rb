02d45a9d38e660d0cb7bedf4724f75b2a5269d11
Lucille James <lucille.james00@gmail.com>
2020-10-01 11:07:09 +0200

    changes to the controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -11,11 +11,11 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.new(author_params)
-    # if @author.save
-    #   redirect_to author_path(@author)
-    # else
-    #   render :new
-    # end
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
   end
 
   def destroy
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,8 +9,17 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+  end
+
   def create
     @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
   end
 
   def destroy
